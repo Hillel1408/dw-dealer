dfd92634b1504466dfbb622732a8eabdd8e679f3
Hillel1408 <etemax@bk.ru>
2023-01-19 18:33:47 +0300

    dw-dealer

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,3 +1,10 @@
 (() => {
+  // src/components/sections/reviews/reviews.js
+  $(document).ready(function() {
+    $(".reviews__slider").slick({
+      dots: false,
+      arrows: false
+    });
+  });
 })();
 //# sourceMappingURL=index.js.map
---
+++ src/components/sections/reviews/reviews.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".reviews__slider").slick({
+    dots: false,
+    arrows: false,
+  });
+});
