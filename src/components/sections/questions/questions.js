f82dc513521f06a9dbb3536f1a43ff62233989e5
Hillel1408 <etemax@bk.ru>
2023-01-20 23:31:10 +0300

    dw-dealer

--- src/components/sections/questions/questions.js
+++ src/components/sections/questions/questions.js
@@ -1,8 +1,8 @@
 import func from "../../../js/func";
 
-let phoneInput = document.querySelector(".phone3");
-let nameInput = document.querySelector(".name3");
-let btn = document.querySelector(".btn4");
+const phoneInput = document.querySelector(".phone3");
+const nameInput = document.querySelector(".name3");
+const btn = document.querySelector(".btn4");
 const popup = document.querySelector(".popup4");
 const succes = document.querySelector(".succes4");
 
